60d2e725fcd67dadaf7ac78316ee931bb5727eb2
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-07 13:08:00 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,7 @@ int main(void)
 printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
 printf("Size of long: %zu bytes\n", sizeof(long));
 printf("Size of long long: %zu bytes\n", sizeof(long long));
-printf("Size of pointer: %zu bytes\n", sizeof(void *));
-	return (0);
+return (0);
 }
