c459f4168a6d89e7a1f1c8d74335058844edc2c0
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-07 13:53:36 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
-#include <sys/syscall.h>
 
 /**
  * main - Entry point
  *
  * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (success)
+ * Return:  1  (not success)
  */
 
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 2, message, 42);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.quo.59);
 return (1);
 }
