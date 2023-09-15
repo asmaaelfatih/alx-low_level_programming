f8d3d464705768e5cd615d72f2d922523c8af836
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-15 16:43:06 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+* main -  Entry point
+*
+* Description: Fizz Buzz FizzBuzz
+*
+* Return: This Return 0
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
