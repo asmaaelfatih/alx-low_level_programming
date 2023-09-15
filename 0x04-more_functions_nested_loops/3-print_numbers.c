e408874efd11e9bae0208976cf7eba9e7890a6fc
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-15 16:11:28 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers  -  Print numbers from 0 to 9
+*
+* Return:  Return void
+*/
+
+void print_numbers(void)
+{
+	char  i = '0';
+
+	while  (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+
