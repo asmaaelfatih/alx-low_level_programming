8ddde62bb777499f9cecaa3eccdd54b18d9fdb99
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-18 12:40:07 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes the pointer to int parameter ant update its value to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
