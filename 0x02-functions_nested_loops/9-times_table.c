a7b815b6fdbbc62351ba2f25d5ab6d8be46f2923
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-20 08:48:29 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,6 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0');
-
 		for (b = 1; b <= 9; b++)
 		{
 			_putchar(',');
@@ -33,6 +32,6 @@ void times_table(void)
 			}
 			_putchar ((c % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
