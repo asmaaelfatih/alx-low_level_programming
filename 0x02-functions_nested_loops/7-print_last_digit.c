7134ef018286de65b937d955f36f452a5035ad20
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-20 07:28:47 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_last_digit -  Print last digit of a number
+*
+* @i: Number to print last digit for
+*
+* Return: Return digit value
+*/
+
+int print_last_digit(int i)
+{
+	int d;
+
+	d = i % 10;
+
+	if (i > 0)
+	{
+		_putchar(d + '0');
+		return (d);
+	}
+	else
+	{
+		_putchar (-1 * d + '0');
+		return (-d);
+	}
+}
