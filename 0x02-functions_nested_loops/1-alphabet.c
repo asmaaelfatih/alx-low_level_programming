a1576b4b4a5178121e979f3cdcf3c201fd5bb10d
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-20 06:30:57 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print alphabet, in lowercase
+*
+* Return: Void
+*/
+
+void print_alphabet(void)
+{
+	char  l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+}
