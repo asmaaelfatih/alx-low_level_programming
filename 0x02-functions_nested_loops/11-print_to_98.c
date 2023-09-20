e62a3451e3b8266cfc2726df00ad5c61c744e031
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-20 08:32:18 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - Prints number to 98
+*
+* @n: Input value
+*
+* Return: Return void
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	}
+	printf("98\n");
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* jack_bauer - Prints every minute of the day of Jack Bauer
+*
+* Return: Return void
+*/
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
