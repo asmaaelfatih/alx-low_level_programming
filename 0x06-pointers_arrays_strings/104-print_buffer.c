eab34fcade1f070f2e8e9d36fa5de81426af28d4
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-21 17:20:11 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,45 +12,5 @@
  */
 void print_buffer(char *b, int size)
 {
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	int i, j;
-
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x ", i);
-		for (j = i; j < i + 10; j += 2)
-		{
-			if (j < size)
-			{
-				printf("%02x", (unsigned char)b[j]);
-
-				if (j + 1 < size)
-				{
-					printf("%02x ", (unsigned char)b[j + 1]);
-				}
-				else
-				{
-					printf("   ");
-				}
-			}
-			else
-			{
-				printf("     ");
-			}
-		}
-		for (j = i; j < i + 10 && j < size; j++)
-		{
-			if (isprint(b[j]))
-			{
-				printf("%c", b[j]);
-			}
-			else
-			{
-				printf(".");
-			}}
-		printf("\n");
-	}}
+	print_buffer(b, size);
+}
