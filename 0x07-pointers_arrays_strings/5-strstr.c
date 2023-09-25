b07c31418491075421fdc0aaa2c2c4a11667d22f
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-25 13:40:59 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include"main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack:
+ * @needle:
+ * Reurn: pointer to the beginning of the located substring, or NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
