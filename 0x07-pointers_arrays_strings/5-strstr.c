8af4b1dfa73866b1acf21981b8243140d248df44
“asmaaelfatihh” <asmaa.elfatihh@gmail.com>
2023-09-25 15:09:14 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,31 @@
 #include"main.h"
 
 /**
- * _strstr - locates a substring
- * @haystack:
- * @needle:
- * Reurn: pointer to the beginning of the located substring, or NULL
+ * *_strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: ointer to the beginning of the located substring
 */
 char *_strstr(char *haystack, char *needle)
 {
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (haystack[itr] != '\0')
+	{
+		jtr = 0;
+		while (needle[jtr] != '\0')
+		{
+			if (needle[jtr] == haystack[itr])
+			{
+				p = &haystack[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++
+	}
+	return (0)
+}
+
